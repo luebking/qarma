6527bbdda26a4b5846d8e07356c4ce809c89dad7
Thomas Lübking <thomas.luebking@gmail.com>
2021-08-07 17:16:32 +0200

    invalid palette group

--- Qarma.cpp
+++ Qarma.cpp
@@ -1183,7 +1183,7 @@ char Qarma::showText(const QStringList &args)
     }
     if (te->isReadOnly()) {
         QPalette pal = te->viewport()->palette();
-        for (int i = 0; i < 4; ++i) { // Disabled, Active, Inactive, Normal
+        for (int i = 0; i < 3; ++i) { // Disabled, Active, Inactive, Normal
             QPalette::ColorGroup cg = (QPalette::ColorGroup)i;
             pal.setColor(cg, QPalette::Base, pal.color(cg, QPalette::Window));
             pal.setColor(cg, QPalette::Text, pal.color(cg, QPalette::WindowText));
