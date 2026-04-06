luebking/qarma

commit 3d1a0c48183077e6d5fac0f222a462784a2f7888

implement output of specific list columns
fixes #58

Qarma.cpp
@@ -549,10 +549,24 @@ void Qarma::dialogFinished(int status)
             QTreeWidget *tw = sender()->findChild<QTreeWidget*>();
             QStringList result;
             if (tw) {
+                QVariant v = sender()->property("qarma_print_column");
+                int column = v.toString() == "ALL" ? -1 : v.toInt();
+                if (column > tw->columnCount())
+                    column = -1;
+                else if (column > 0)
+                    --column;
                 bool done(false);
                 foreach (const QTreeWidgetItem *twi, tw->selectedItems()) {
                     done = true;
-                    result << twi->text(0);
+                    QString s;
+                    if (column > -1)
+                        s = twi->text(column);
+                    else {
+                        for (int i = 0; i < tw->columnCount()-1; ++i)
+                            s += twi->text(i) + '\t';
+                        s += twi->text(tw->columnCount()-1);
+                    }
+                    result << s;
                 }
                 if (!done) { // checkable
                     for (int i = 0; i < tw->topLevelItemCount(); ++i) {
@@ -1012,7 +1026,7 @@ char Qarma::showList(const QStringList &args)
             if (ok)
                 hiddenCols << v-1;
         } else if (args.at(i) == "--print-column") {
-            qWarning("TODO: --print-column");
+            dlg->setProperty("qarma_print_column", NEXT_ARG);
         } else if (args.at(i) == "--checklist") {
             tw->setSelectionMode(QAbstractItemView::NoSelection);
             tw->setAllColumnsShowFocus(false);

commit 2aab617b5bdfa4e622296be703d4fda9b5f5738f

handle default print column
fixes #66

bug introduced in 3d1a0c48

Qarma.cpp
@@ -560,11 +560,14 @@ void Qarma::dialogFinished(int status)
             QStringList result;
             if (tw) {
                 QVariant v = sender()->property("qarma_print_column");
-                int column = v.toString() == "ALL" ? -1 : v.toInt();
-                if (column > tw->columnCount())
-                    column = -1;
-                else if (column > 0)
-                    --column;
+                int column = 1;
+                if (v.isValid()) {
+                    column = v.toString() == "ALL" ? -1 : v.toInt();
+                    if (column > tw->columnCount())
+                        column = -1;
+                    else if (column > 0)
+                        --column;
+                }
                 bool done(false);
                 auto text = [=](const QTreeWidgetItem *twi, int col, int offset) {
                     if (col > -1)